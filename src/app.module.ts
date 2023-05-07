a6f77b4614be11d602d9632cefc0573fdf68bf0d
Swain <>
2023-05-07 20:43:08 +0800

    [feature] src/middleware, implement logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,16 @@ import { LineBotModule } from './line-bot/line-bot.module';
 import { Config } from './config/config.module';
 import { ThirdPartyModule } from './third-party/third-party.module';
 import { ChatGPTModule } from './chat-gpt/chat-gpt.module';
+import { NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [LineBotModule, Config, ThirdPartyModule, ChatGPTModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
